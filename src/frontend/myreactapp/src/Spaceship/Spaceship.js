1676bb77dd4e1645f58b69d99c055805645fc2b3
Mike Møller Nielsen <ekim197711@gmail.com>
2019-09-18 21:26:55 +0200

    Events and state

--- src/frontend/myreactapp/src/Spaceship/Spaceship.js
+++ src/frontend/myreactapp/src/Spaceship/Spaceship.js
@@ -2,7 +2,7 @@ import React from 'react'
 import './Spaceship.css'
 const spaceship = (props) => {
     return (
-        <div className='SpaceshipLAF'>
+        <div className='SpaceshipLAF' onClick={props.myclickhandler}>
             {props.name} - {props.crew} - {props.children}
         </div>
     )
--- src/frontend/myreactapp/src/SpaceshipList/SpaceshipList.js
+++ src/frontend/myreactapp/src/SpaceshipList/SpaceshipList.js
@@ -3,20 +3,41 @@ import Spaceship from "../Spaceship/Spaceship";
 
 class SpaceshipList extends Component {
     state = {
-        anotherheader: 'Some value'
+        anotherheader: 'Some value',
+        spaceships: [
+            {id: 0, name: 'Spaceship1', crew: 150},
+            {id: 1, name: 'Spaceship2', crew: 220},
+            {id: 2, name: 'Spaceship3', crew: 30},
+            {id: 3, name: 'Spaceship4', crew: 40},
+            {id: 4, name: 'Spaceship5', crew: 50},
+            {id: 5, name: 'Spaceship6', crew: 330},
+            {id: 6, name: 'Spaceship7', crew: 120},
+        ]
+    };
+
+    deleteAShip = (id) => {
+        const copyofships = [...this.state.spaceships];
+        console.log("Before deletion: " + JSON.stringify(copyofships))
+        const indexofship = copyofships.findIndex(ship => ship.id === id)
+        copyofships.splice(indexofship,1);
+        console.log("After deletion: " + JSON.stringify(copyofships))
+        this.setState({anotherheader: 'State has been set!!!',
+        spaceships: [...copyofships]});
     }
 
     render() {
+        const ships = this.state.spaceships.map(spaceship => (
+            <Spaceship name={spaceship.name}
+                       myclickhandler={this.deleteAShip.bind(this, spaceship.id)}
+                       crew={spaceship.crew}  key={spaceship.id} id={spaceship.id} />
+            )
+        );
+
         return (
             <div>
                 <h1>{this.props.myheader}</h1>
                 <h2>{this.state.anotherheader}</h2>
-                <Spaceship name='Hawk' crew='230'>Slightly damaged</Spaceship>
-                <Spaceship name='Falcon' crew='430'>Brand new</Spaceship>
-                <Spaceship name='Blackbird' crew='100'>Needs refueling</Spaceship>
-                <Spaceship name='Stork' crew='50'/>
-                <Spaceship name='Parakite' crew='1000'/>
-                <Spaceship name='Parrot' crew='55'/>
+                {ships}
             </div>
         )
     }
